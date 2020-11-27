d67f13ed90965c044ec2eb76960cbd35c10e8ac0
lg <leegcc@users.noreply.github.com>
2020-11-28 00:42:21 +0800

    方糖通知

--- ftqq.js
+++ ftqq.js
@@ -2,8 +2,10 @@ const got = require('got');
 module.exports = async function (message) {
   const key = process.env.FT_SCKEY;
   if (key == null) {
-    console.info(`未配置 FT_SCKEY，不发送方糖通知`)
+    console.info(`未配置 FT_SCKEY，不发送方糖通知`);
     return;
   }
+
+  console.info(`准备发送方糖通知`);
   return got(`https://sc.ftqq.com/${key}.send?text=${message}`);
 };
